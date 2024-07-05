ad4c906b493d4cd3c76abf0c01a79f0ee194e15c
Eva De Haro <eva@dallonses.com>
2024-07-05 20:07:36 +0200

    feat: updated menu layout example

--- app/page.js
+++ app/page.js
@@ -1,113 +1,155 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react";
 
 export default function Home() {
+  const [menuOpened, setMenuOpened] = useState(undefined)
+  const [submenuHovered, setSubmenuOpened] = useState(undefined)
+
+  function closing() {
+    setMenuOpened(undefined)
+    setSubmenuOpened(undefined)
+  }
+
+  function opening(numberOpt) {
+    setMenuOpened(numberOpt)
+    setSubmenuOpened(undefined)
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <header className="bg-white fixed top-0 left-0 px-4 pt-4 w-full text-black">
+        <nav onMouseLeave={() => closing()}>
+          <ul className="flex gap-28 justify-center z-50 border-b pb-4 grid grid-cols-12 gap-x-4">
+            <li className="col-span-2 col-start-4">
+              <button onMouseEnter={() => opening("1")}>Option 1</button>
+            </li>
+            <li className="col-span-2 col-start-6">
+              <button onMouseEnter={() => opening("2")}>Option 2</button>
+            </li>
+            <li className="col-span-2 col-start-8">
+              <button onMouseEnter={() => opening("3")}>Option 3</button>
+            </li>
+          </ul>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          {menuOpened === "1" &&
+            <nav className="h-[40vh] w-full bg-white z-0 grid grid-cols-12 gap-x-4 pt-14">
+              <ul className="col-span-2 col-start-4">
+                <li>
+                  Link 1
+                </li>
+                <li onMouseEnter={() => setSubmenuOpened("hoveredItem")}>
+                  <div className="flex justify-between items-center">
+                    <p>Hover me</p>
+                    <svg width="7" height="11" viewBox="0 0 7 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M1 1L6 5.32L1 10" stroke="#002391" />
+                    </svg>
+                  </div>
+                </li>
+                <li>
+                  Link 2
+                </li>
+              </ul>
+              {submenuHovered &&
+                <ul className="col-span-2 col-start-6">
+                  <li>
+                    Link 1
+                  </li>
+                  <li onMouseEnter={() => setSubmenuOpened("hoveredItem")}>
+                    Link 2
+                  </li>
+                </ul>
+              }
+            </nav>
+          }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {menuOpened === "2" &&
+            <nav className="h-[40vh] w-full bg-white z-0 grid grid-cols-12 gap-x-4 pt-14">
+              <ul className="col-span-2 col-start-6">
+                <li>
+                  Link 1
+                </li>
+                <li onMouseEnter={() => setSubmenuOpened("hoveredItem")}>
+                  <div className="flex justify-between items-center">
+                    <p>Hover me</p>
+                    <svg width="7" height="11" viewBox="0 0 7 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M1 1L6 5.32L1 10" stroke="#002391" />
+                    </svg>
+                  </div>
+                </li>
+                <li>
+                  Link 2
+                </li>
+              </ul>
+              {submenuHovered &&
+                <ul className="col-span-2 col-start-8 max-h-[60%] overflow-hidden overflow-y-auto">
+                  <li>
+                    Link 1
+                  </li>
+                  <li onMouseEnter={() => setSubmenuOpened("hoveredItem")}>
+                    Link 2
+                  </li>
+                  <li>
+                    Link 3
+                  </li>
+                  <li onMouseEnter={() => setSubmenuOpened("hoveredItem")}>
+                    Link 4
+                  </li>
+                  <li>
+                    Link 5
+                  </li>
+                  <li onMouseEnter={() => setSubmenuOpened("hoveredItem")}>
+                    Link 6
+                  </li>
+                  <li>
+                    Link 7
+                  </li>
+                  <li onMouseEnter={() => setSubmenuOpened("hoveredItem")}>
+                    Link 8
+                  </li>
+                  <li>
+                    Link 9
+                  </li>
+                  <li onMouseEnter={() => setSubmenuOpened("hoveredItem")}>
+                    Link 10
+                  </li>
+                </ul>
+              }
+            </nav>
+          }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          {menuOpened === "3" &&
+            <nav className="h-[40vh] w-full bg-white z-0 grid grid-cols-12 gap-x-4 pt-14">
+              <ul className="col-span-2 col-start-8">
+                <li>
+                  Link 1
+                </li>
+                <li onMouseEnter={() => setSubmenuOpened("hoveredItem")}>
+                  <div className="flex justify-between items-center">
+                    <p>Hover me</p>
+                    <svg width="7" height="11" viewBox="0 0 7 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M1 1L6 5.32L1 10" stroke="#002391" />
+                    </svg>
+                  </div>
+                </li>
+                <li>
+                  Link 2
+                </li>
+              </ul>
+              {submenuHovered &&
+                <ul className="col-span-2 col-start-10">
+                  <li>
+                    Link 1
+                  </li>
+                  <li onMouseEnter={() => setSubmenuOpened("hoveredItem")}>
+                    Link 2
+                  </li>
+                </ul>
+              }
+            </nav>
+          }
+        </nav>
+      </header>
     </main>
   );
 }
